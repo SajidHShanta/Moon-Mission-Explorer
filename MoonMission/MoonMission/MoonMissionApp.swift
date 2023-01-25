7fa92655e386228b056ccd2670daa307c98e7b66
sajid <sajidhasan673@gmail.com>
2023-01-25 12:17:42 +0600

    setup app and add assets (images and JSON data)

---
+++ MoonMission/MoonMission/MoonMissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonMissionApp.swift
+//  MoonMission
+//
+//  Created by Sajid Shanta on 25/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonMissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
