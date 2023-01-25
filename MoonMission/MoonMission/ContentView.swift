d28e66bd518f7f4b07c1a2e6c8394bb86b55f6a4
sajid <sajidhasan673@gmail.com>
2023-01-25 14:27:31 +0600

    create and format mission view

--- MoonMission/MoonMission/ContentView.swift
+++ MoonMission/MoonMission/ContentView.swift
@@ -12,11 +12,51 @@ struct ContentView: View {
         let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
         let missions: [Mission] = Bundle.main.decode("missions.json")
         
-        VStack {
-            Text("\(astronauts.count)")
-            Text("\(missions.count)")
+        let columns = [
+            GridItem(.adaptive(minimum: 150))
+        ]
+        
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text(mission.description)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moon Mission")
+            .preferredColorScheme(.dark)
+            .background(.darkBackground)
         }
-        .padding()
     }
 }
 
